6ea0f52371bff384713de2fb61147a62f9f54c2e
yogoloper <jguddnjs@gmail.com>
2024-02-24 19:04:56 +0900

    클래스에서의 state 사용

--- src/App.js
+++ src/App.js
@@ -5,25 +5,40 @@ function App() {
   return (
     <div className="container">
       <h1>Hello world!</h1>
-      <FuncComp></FuncComp>
-      <ClassComp></ClassComp>
+      <FuncComp initNumber={2}></FuncComp>
+      <ClassComp initNumber={2}></ClassComp>
     </div>
   );
 }
 
-function FuncComp() {
+function FuncComp(props) {
+
   return ( 
     <div className="container">
       <h2>function style component</h2>
+      <p>props Number: {props.initNumber}</p>
     </div>
   )
 }
 
 class ClassComp extends Component {
+  state = {
+    number: this.props.initNumber
+  }
   render() {
     return (
       <div className="container">
         <h2>class style component</h2>
+        <p>props Number: {this.props.initNumber}</p>
+        <p>state Number: {this.state.number}</p>
+        <input 
+          type='button'
+          value='random'
+          onClick={function() {
+            this.setState({
+              number: Math.random()
+            })
+          }.bind(this)} />
       </div>
     )
   }
