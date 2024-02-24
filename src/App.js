0893abf19d49469fc57e76128e948a4d20db82aa
yogoloper <jguddnjs@gmail.com>
2024-02-24 19:17:02 +0900

    함수에서 state 사용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -12,18 +12,37 @@ function App() {
 }
 
 function FuncComp(props) {
+  const [number, setNumber] = useState(props.initNumber);
+  const [date, setDate] = useState((new Date()).toString());
 
   return ( 
     <div className="container">
       <h2>function style component</h2>
       <p>props Number: {props.initNumber}</p>
+      <p>state Number: {number}</p>
+      <p>date: {date}</p>
+      <input 
+        type='button'
+        value='random'
+        onClick={function() {
+          setNumber(Math.random())
+        }}
+      />
+      <input 
+        type='button'
+        value='date'
+        onClick={function() {
+          setDate((new Date()).toString())
+        }}
+      />
     </div>
   )
 }
 
 class ClassComp extends Component {
   state = {
-    number: this.props.initNumber
+    number: this.props.initNumber,
+    date: (new Date()).toString()
   }
   render() {
     return (
@@ -31,12 +50,21 @@ class ClassComp extends Component {
         <h2>class style component</h2>
         <p>props Number: {this.props.initNumber}</p>
         <p>state Number: {this.state.number}</p>
+        <p>date: {this.state.date}</p>
         <input 
           type='button'
           value='random'
           onClick={function() {
             this.setState({
               number: Math.random()
+            })
+          }.bind(this)} />
+                <input 
+          type='button'
+          value='date'
+          onClick={function() {
+            this.setState({
+              date: (new Date()).toString()
             })
           }.bind(this)} />
       </div>
