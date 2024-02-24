1a5c999bea584f7611aaf7cdc3db053abe45213a
yogoloper <jguddnjs@gmail.com>
2024-02-24 18:57:31 +0900

    클래스, 함수 컴포넌트 각각 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import { Component } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="container">
+      <h1>Hello world!</h1>
+      <FuncComp></FuncComp>
+      <ClassComp></ClassComp>
     </div>
   );
+}
+
+function FuncComp() {
+  return ( 
+    <div className="container">
+      <h2>function style component</h2>
+    </div>
+  )
+}
+
+class ClassComp extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>class style component</h2>
+      </div>
+    )
+  }
 }
 
 export default App;
